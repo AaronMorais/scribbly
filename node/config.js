27abc8a5e4992705f0c07ec4589df5253a883ed7
Kevin Bedi <kevin@kevinbedi.com>
2013-09-28 14:05:11 -0700

    Config/first endpoint

---
+++ node/config.js
@@ -0,0 +1,15 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'hackathon',
+});
+
+var query = function(query, cb) {
+    connection.connect();
+    connection.query(query, cb);
+    connection.end()
+}
+
+
+exports.query = query;
--- node/saveNote.js
+++ node/saveNote.js
@@ -1,6 +1,13 @@
-var db = require("./config.js").db;
+var config = require("./config.js");
 
 var saveNote = function(req, res) {
     var text = req.param("text");
+    var id = req.param("id");
 
+    if (!text) return;
+
+    // Update
+    if (id) {
+        
+    }
 }
