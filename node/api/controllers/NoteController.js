8f288e6e93117dc055a367cf4c0ff07d72aba0c5
Kevin Bedi <kevin@kevinbedi.com>
2013-09-29 12:16:00 -0700

    logs

--- node/api/controllers/NoteController.js
+++ node/api/controllers/NoteController.js
@@ -139,6 +139,9 @@ module.exports = {
                 for (var x in stns) {
                     (function(stn, index) {
                         Note.findOne({id: stn.note, user: user.id}).done(function(err, note) {
+                            cosole.log(stn.synonym);
+                            console.log(query);
+                            console.log(note);
                             if (note && (stn.synonym.toLowerCase().indexOf(query.toLowerCase()) != -1 || note.text.toLowerCase().indexOf(query.toLowerCase()) != -1) && !_.findWhere(notes, {id: note.id}))
                                 notes.push(note);
                             if (index == stns.length-1) {
