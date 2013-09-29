558aaaf223885371905429dffbf13e89334a6c9d
Kevin Bedi <kevin@kevinbedi.com>
2013-09-29 10:00:38 -0700

    smexy colrz

--- iOS/Scribly/Scribly/SCRCategoryGridViewController.m
+++ iOS/Scribly/Scribly/SCRCategoryGridViewController.m
@@ -166,20 +166,28 @@
     NSArray *colors;
     if (seed % 4 == 0) {
         colors = @[
-            [UIColor colorWithRed:52.0f/256.0f green:73.0f/256.0f blue:94.0f/256.0f alpha:1.0f],
-            [UIColor colorWithRed:39.0f/256.f green:174.0f/256.0f blue:96.0f/256.0f alpha:1.0f]];
-    } else if(seed % 4 == 1) {
-        colors = @[
-            [UIColor colorWithRed:41.0f/256.0f green:128.0f/256.0f blue:185.0f/256.0f alpha:1.0f],
-            [UIColor colorWithRed:46.0f/256.0f green:204.0f/256.0f blue:113.0f/256.0f alpha:1.0f]];
-    } else if(seed % 4 == 2) {
+            [UIColor colorWithRed:231.0f/256.0f green:76.0f/256.0f blue:60.0f/256.0f alpha:1.0f],
+            [UIColor colorWithRed:230.0f/256.0f green:126.0f/256.0f blue:34.0f/256.0f alpha:1.0f],
+            [UIColor colorWithRed:241.0f/256.0f green:196.0f/256.0f blue:15.0f/256.0f alpha:1.0f],
+            [UIColor colorWithRed:155.0f/256.f green:89.0f/256.0f blue:182.0f/256.0f alpha:1.0f]];
+    } else (seed % 4 == 1) {
         colors = @[
             [UIColor colorWithRed:192.0f/256.0f green:57.0f/256.0f blue:43.0f/256.0f alpha:1.0f],
-            [UIColor colorWithRed:230.0f/256.0f green:126.0f/256.0f blue:34.0f/256.0f alpha:1.0f]];
+            [UIColor colorWithRed:230.0f/256.0f green:126.0f/256.0f blue:34.0f/256.0f alpha:1.0f],
+            [UIColor colorWithRed:243.0f/256.0f green:156.0f/256.0f blue:18.0f/256.0f alpha:1.0f],
+            [UIColor colorWithRed:142.0f/256.0f green:68.0f/256.0f blue:173.0f/256.0f alpha:1.0f]];
+    } else (seed % 4 == 2) {
+        colors = @[
+            [UIColor colorWithRed:52.0f/256.0f green:152.0f/256.0f blue:219.0f/256.0f alpha:1.0f],
+            [UIColor colorWithRed:46.0f/256.0f green:204.0f/256.0f blue:113.0f/256.0f alpha:1.0f],
+            [UIColor colorWithRed:26.0f/256.0f green:188.0f/256.0f blue:156.0f/256.0f alpha:1.0f],
+            [UIColor colorWithRed:22.0f/256.0f green:160.0f/256.0f blue:133.0f/256.0f alpha:1.0f]];    
     } else {
         colors = @[
-            [UIColor colorWithRed:44.0f/256.0f green:62.0f/256.0f blue:80.0f/256.0f alpha:1.0f],
-            [UIColor colorWithRed:52.0f/256.0f green:152.0f/256.0f blue:219.0f/256.0f alpha:1.0f]];
+            [UIColor colorWithRed:41.0f/256.0f green:128.0f/256.0f blue:185.0f/256.0f alpha:1.0f],
+            [UIColor colorWithRed:39.0f/256.0f green:174.0f/256.0f blue:96.0f/256.0f alpha:1.0f],
+            [UIColor colorWithRed:22.0f/256.0f green:160.0f/256.0f blue:133.0f/256.0f alpha:1.0f],
+            [UIColor colorWithRed:44.0f/256.0f green:62.0f/256.0f blue:80.0f/256.0f alpha:1.0f]];
     }
     uint32_t rnd = arc4random_uniform([colors count]);
     return [colors objectAtIndex:rnd];
